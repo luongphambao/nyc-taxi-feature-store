cdf1ba9926c702bff574c318c68c6baa7282f6dd
Luong Pham Bao <58712033+19521242bao@users.noreply.github.com>
2024-01-03 20:01:04 +0700

    update python3 code for drop table

---
+++ src/drop_table.py
@@ -0,0 +1,28 @@
+import os
+import pandas as pd
+from glob import glob
+from dotenv import load_dotenv
+from postgresql_client import PostgresSQLClient
+
+load_dotenv()
+
+
+def main():
+    pc = PostgresSQLClient(
+        database=os.getenv("POSTGRES_DB"),
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD"),
+    )
+
+    # Create devices table
+    drop_table_query = """
+        DROP TABLE IF EXISTS public.nyc_taxi;
+    """
+    try:
+        pc.execute_query(drop_table_query)
+    except Exception as e:
+        print(f"Failed to create table with error: {e}")
+
+
+if __name__ == "__main__":
+    main()
