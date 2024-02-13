ecccb623b11a197ce04e56da50ec0606f09143ff
Luong Pham Bao <58712033+luongphambao@users.noreply.github.com>
2024-02-13 14:14:31 +0700

    update pipeline and fix tiny bugs

--- airflow/dags/data2.py
+++ airflow/dags/data2.py
@@ -8,22 +8,27 @@ from airflow.providers.postgres.hooks.postgres import PostgresHook
 from great_expectations_provider.operators.great_expectations import (
     GreatExpectationsOperator,
 )
+import pandas as pd
+import os
 DATA_DIR="/opt/airflow/data/"
+POSTGRES_CONN_ID = "postgres_default"   
 def insert_table():
-    import pandas as pd
-    import os
     pg_hook = PostgresHook.get_hook(conn_id=POSTGRES_CONN_ID)
-    # for file in os.listdir(DATA_DIR):
-    #     if file.endswith(".parquet"):
-    #         df=pd.read_parquet(os.path.join(DATA_DIR,file))
-    #         #drop store_and_fwd_flag and airport_fee
-    #         df=df.drop(columns=["store_and_fwd_flag","airport_fee"])
-    #         print("Inserting data from file: "+file)
-    df=pd.read_parquet("/opt/airflow/data/yellow_tripdata_2021-03.parquet")
-    df=df.drop(columns=["store_and_fwd_flag","airport_fee"])
-    pg_hook.insert_rows(table="nyc_taxi", rows=df.values.tolist())
-POSTGRES_CONN_ID = "postgres_default"
-with DAG(dag_id="nyc_taxi_datawarehouse", start_date=datetime(2023, 7, 1), schedule=None) as dag:
+    for file in os.listdir(DATA_DIR):
+        if file.endswith(".parquet") and str(file).startswith("yellow"):
+            df=pd.read_parquet(os.path.join(DATA_DIR,file))
+            #drop store_and_fwd_flag and airport_fee
+            #df=df.drop(columns=["store_and_fwd_flag","airport_fee"])
+            print("Inserting data from file: "+file)
+            #get 1000 sample from df
+            df=df.sample(1000)
+            #print number of columns
+            print(df.shape)
+            pg_hook.insert_rows(table="nyc_taxi", rows=df.values.tolist())
+    #df=pd.read_parquet("/opt/airflow/data/yellow_tripdata_2021-03.parquet")
+    
+
+with DAG(dag_id="nyc_taxi2", start_date=datetime(2023, 7, 1), schedule=None) as dag:
     # This is often used for tasks which are more suitable for executing commands
     # For example, submit a job to a Spark cluster, initiate a new cluster,
     # run containers, upgrade software packages on Linux systems,
@@ -32,13 +37,14 @@ with DAG(dag_id="nyc_taxi_datawarehouse", start_date=datetime(2023, 7, 1), sched
         task_id="create_table_pg",
         postgres_conn_id=POSTGRES_CONN_ID,
         sql = """
-        CREATE TABLE IF NOT EXISTS nyc_taxi (
+        CREATE TABLE IF NOT EXISTS nyc_taxi(
             vendorid  INT, 
             tpep_pickup_datetime TIMESTAMP WITHOUT TIME ZONE, 
             tpep_dropoff_datetime TIMESTAMP WITHOUT TIME ZONE, 
             passenger_count FLOAT, 
             trip_distance FLOAT, 
-            ratecodeid FLOAT,  
+            ratecodeid FLOAT, 
+            store_and_fwd_flag VARCHAR(1), 
             pulocationid INT, 
             dolocationid INT, 
             payment_type INT, 
@@ -49,7 +55,8 @@ with DAG(dag_id="nyc_taxi_datawarehouse", start_date=datetime(2023, 7, 1), sched
             tolls_amount FLOAT, 
             improvement_surcharge FLOAT, 
             total_amount FLOAT, 
-            congestion_surcharge FLOAT
+            congestion_surcharge FLOAT,
+            airport_fee FLOAT
         );
         
     """
