0cb2e1ef764b2c0fb309fcc58c264b70858addb5
Luong Pham Bao <58712033+luongphambao@users.noreply.github.com>
2024-02-16 23:09:32 +0700

    refactor code

--- src/batch_processing.py
+++ src/batch_processing.py
@@ -12,25 +12,26 @@ def main():
         SparkSession.builder.master("local[*]")
         .config(
             "spark.jars",
-            "jars/postgresql-42.6.0.jar,jars/deequ-2.0.3-spark-3.3.jar,jars/trino-jdbc-434.jar",
+            "jars/postgresql-42.4.3.jar,jars/deequ-2.0.3-spark-3.3.jar,jars/trino-jdbc-434.jar",
         )
         .appName("Python Spark SQL basic example")
         .config("spark.some.config.option", "some-value")
         .getOrCreate()
     )
+    print("Spark session created")
     # Read from PostgreSQL database via jdbc connection
     # https://spark.apache.org/docs/latest/sql-data-sources-jdbc.html
     print(os.getenv("TRINO_DBTABLE"))
     print(os.getenv("TRINO_USER"))
     print(os.getenv("TRINO_PASSWORD"))
     #select * from datalake.taxi_time_series.nyc_taxi 
-
+    print(os.getenv('TRINO_PORT'))
     df = (
         spark.read.format("jdbc")
         .option("driver", "io.trino.jdbc.TrinoDriver")
         .option("url", f"jdbc:trino://localhost:{os.getenv('TRINO_PORT')}/")
-        .option("dbtable", os.getenv("TRINO_DBTABLE"))
-        .option("user", os.getenv("TRINO_USER"))
+        .option("dbtable", os.getenv("TRINO_DBTABLE","datalake.taxi_time_series.nyc_taxi"))
+        .option("user", os.getenv("TRINO_USER","trino"))
         .option("password", os.getenv("TRINO_PASSWORD",""))
         .load()
     )
