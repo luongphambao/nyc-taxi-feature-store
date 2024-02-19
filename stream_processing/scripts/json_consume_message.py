3fe40f11e43854ad9230372de827f0c0bb1c4e96
Luong Pham Bao <58712033+luongphambao@users.noreply.github.com>
2024-02-19 19:56:19 +0700

    refactor code

--- stream_processing/scripts/json_consume_message.py
+++ stream_processing/scripts/json_consume_message.py
@@ -1,14 +1,23 @@
 from confluent_kafka import Consumer, KafkaException
 import json
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--topic', type=str, default='nyc_taxi.public.nyc_taxi', help='topic to consume')
+    args = parser.parse_args()
+    return args
 
 def main():
+    args = parse_args()
+    topic = args.topic
     consumer = Consumer({
         'bootstrap.servers': 'localhost:9092',
         'group.id': 'mygroup',
         'auto.offset.reset': 'earliest' # try latest to get the recent value
     })
 
-    consumer.subscribe(['nyc_taxi.public.nyc_taxi'])
+    consumer.subscribe([topic])
 
     # Read messages from Kafka
     try:
@@ -22,9 +31,13 @@ def main():
                 raise KafkaException(msg.error())
             else:
                 # Parse data from our message
-                value = json.loads(
-                    msg.value().decode('utf-8')
-                )["payload"]["after"]
+                print(f"Received message: {msg.value().decode('utf-8')}")
+                try:
+                    value = json.loads(
+                        msg.value().decode('utf-8')
+                    )["payload"]["after"]
+                except:
+                    value=msg.value().decode('utf-8')
                 print(f"Received message: {value}")
     except KeyboardInterrupt:
         print('Aborted by user!\n')
